8d78466c4928478fea7e0faf052ca95eabff9df2
slubisi <slubisi@esri-southafrica.com>
2015-11-12 09:34:25 +0200

    Create fileupload.js

---
+++ fileupload.js
@@ -0,0 +1,7 @@
+  var uploader = new dojox.form.FileUploader({
+    hoverClass:"uploadHover",
+    activeClass:"uploadBtn",
+    pressClass:"uploadPress",
+    disabledClass:"uploadDisable",
+    uploadUrl:pathToUploadServerScript
+}, "btn");
