ca9e7e45de0c9eaa63be06d87553d092d76080c8
slubisi <slubisi@esri-southafrica.com>
2015-11-12 09:40:35 +0200

    Update fileupload.js

--- fileupload.js
+++ fileupload.js
@@ -3,5 +3,5 @@
     activeClass:"uploadBtn",
     pressClass:"uploadPress",
     disabledClass:"uploadDisable",
-    uploadUrl:pathToUploadServerScript
+    uploadUrl:https://github.com/slubisi/demo
 }, "btn");
